ee33f52685d646efb4abab5acfb66f0ccc1a9c95
Matias Gimenez <p.matias.gimenez@hotmail.es>
2024-10-18 04:50:35 -0300

    primer commit base

---
+++ index.js
@@ -0,0 +1,9 @@
+precio = 400000
+//precio = 10
+
+precioSpan = document.querySelector(".precio-inicial")
+valorinicial = document.querySelector('.valor-total')
+
+precioSpan.innerHTML = precio
+valorinicial.innerHTML = 0
+
